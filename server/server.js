d5b75996090b8391462c5ca8f89c64cc75a49437
Shivam21512 <ugaleshivam73@gmail.com>
2025-07-24 00:50:31 +0530

    backend start

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+//Initialize Express App
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send("Server is running")
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>{
+    console.log(`server running on port ${PORT}`)
+})
