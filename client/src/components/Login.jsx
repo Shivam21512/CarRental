8dbd77c1cb13777ae95fc0d9133316556b43f9b0
Shivam21512 <ugaleshivam73@gmail.com>
2025-07-28 17:11:42 +0530

    completed

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -44,18 +44,18 @@ const Login = () => {
             {state === "register" && (
                 <div className="w-full">
                     <p>Name</p>
-                    <input onChange={(e) => setName(e.target.value)} value={name} placeholder="type here" 
+                    <input onChange={(e) => setName(e.target.value)} value={name} placeholder="Jhon sheth" 
                     className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary" type="text" required />
                 </div>
             )}
             <div className="w-full ">
                 <p>Email</p>
-                <input onChange={(e) => setEmail(e.target.value)} value={email} placeholder="type here"
+                <input onChange={(e) => setEmail(e.target.value)} value={email} placeholder="you@example.com"
                  className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary" type="email" required />
             </div>
             <div className="w-full ">
                 <p>Password</p>
-                <input onChange={(e) => setPassword(e.target.value)} value={password} placeholder="type here" 
+                <input onChange={(e) => setPassword(e.target.value)} value={password} placeholder="••••••••" 
                 className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary" type="password" required />
             </div>
             {state === "register" ? (
